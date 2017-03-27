653fc193867a8816eeed004b822a97623b194947
Jamie Cerexhe <jcerexhe@gmail.com>
2017-03-27 15:12:43 +1100

    added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -10,6 +10,11 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    # To set up the variable for our comment form
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    # Returns an array with all comments that relate to the post
+    @comments = @post.comments
   end
 
   # GET /posts/new
